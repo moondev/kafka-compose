3e67d7b0a1952f4f4938cba61a6e1be8294e6929
Chad Moon <chad.d.moon@gmail.com>
2017-04-12 14:20:03 -0600

    init

---
+++ exec.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#docker exec ktest_kafka-client_1 ./kafka-topics.sh --create --zookeeper zookeeper:2181 --replication-factor 1 --partitions 1 --topic test
+
+#docker exec ktest_kafka-client_1 ./kafka-topics.sh --list --zookeeper zookeeper:2181
